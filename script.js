330c3ebacb395ace9b4e11e153ded8a9ab54b270
paulUG <113113861+paulUG@users.noreply.github.com>
2025-03-12 13:33:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ document.getElementById('login-link').addEventListener('click', (e) => {
     document.getElementById('login-form').classList.add('d-none');
     document.getElementById('signup-form').classList.remove('d-none');
   });
+  document.getElementById('login-link').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('signup-form').classList.add('d-none');
+    document.getElementById('login-form').classList.remove('d-none');
+  });
+  
+  document.getElementById('signup-link').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('login-form').classList.add('d-none');
+    document.getElementById('signup-form').classList.remove('d-none');
+  });
+  function collapseNavbar() {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+      navbarCollapse.classList.remove('show'); // Collapse the navbar
+      const navbarToggler = document.querySelector('.navbar-toggler');
+      navbarToggler.click(); // Simulate a click on the toggle button to close the menu
+    }
+  }
+  // Collapse navbar on scroll
+  window.addEventListener('scroll', () => {
+    collapseNavbar();
+  });
+  // Collapse navbar when a menu option is clicked
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      collapseNavbar();
+    });
+  });
